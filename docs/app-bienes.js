5433dd47833b0fe68424909b0432bac960f397c5
Marco J <marco.jaramillocast@gmail.com>
2020-06-28 11:08:36 -0500

    hompage hamburguer con docs

---
+++ docs/app-bienes.js
@@ -0,0 +1,19 @@
+document.getElementById('boton-ham').onclick = function(animar){  
+document.getElementById("enlacesjs").classList.toggle( "menu-crece");
+}
+
+/*
+document.getElementById('boton').onclick = function(animar){
+document.getElementById("enlaces").classList.toggle( "dos");
+}
+*/
+/*
+function animar() {
+    document.getElementById("enlaces").classList.toggle( "dos");
+}
+    document.getElementById('boton').onclick = function(){
+    animar();
+}
+
+*/
+
